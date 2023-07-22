b1f48cdf09ae0da5f6c112e8de70a62f177df8fa
Muhammed Shamsudeen <muhdsdeen@hikima.ai>
2023-07-22 11:14:59 +0100

    update push

--- deploy.sh
+++ deploy.sh
@@ -5,17 +5,17 @@ apt-get update
 apt-get install -y libsndfile1 espeak-ng libportaudio2
 
 # Set up a virtual environment
-python -m venv venv
-source venv/bin/activate
+#python -m venv venv
+#source venv/bin/activate
 
 # Install additional Python packages from a custom package list
 # Replace "extra_package_1" and "extra_package_2" with the names of your additional packages
 #pip install --no-cache-dir extra_package_1 extra_package_2
 
 # Install main Python dependencies from requirements.txt
-pip install --no-cache-dir -r requirements.txt
+#pip install --no-cache-dir -r requirements.txt
 
 # Additional custom commands or setup steps can be added here
 
 # Start your application
-python app.py
+#python app.py
